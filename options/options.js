598d4118711ce7a797e17a50a2004f427d132433
Crowdbotics <team@crowdbotics.com>
2023-05-06 04:40:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fastest-chugging-ap-40491",
+  url: "https://fastest-chugging-ap-40491.botics.co",
+  api: "https://fastest-chugging-ap-40491.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
